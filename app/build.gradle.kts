d97bac97a68a5e44d065b77c088c9fc2d85df814
Tony Sebastian <107680243+tonysebastine@users.noreply.github.com>
2026-06-26 23:46:55 +0530

    build: update application ID to official domain

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.streamcast.kfjwyv"
+    applicationId = "in.thbz.streamcast"
     minSdk = 24
     targetSdk = 36
     versionCode = 3
